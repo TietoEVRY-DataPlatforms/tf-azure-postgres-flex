0d68e55a5284e8d9111e3069ad903f80cf33a9b4
Marcel Grygar <marcel.grygar@tietoevry.com>
2022-10-27 13:29:17 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+locals {
+  default_charset   = "UTF8"
+  default_collation = "English_United States.1252"
+}
+
+resource "azurerm_postgresql_flexible_server" "postgresql_server" {
+  name                   = var.name
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  administrator_login    = var.administrator_login
+  administrator_password = var.administrator_password
+  version                = var.version
+  storage_mb             = var.storage_mb
+  sku_name               = var.sku_name
+  zone                   = var.zones
+
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+
+
+  delegated_subnet_id = var.delegated_subnet_id
+  private_dns_zone_id = var.private_dns_zone_id
+
+
+  tags               = var.tags
+  high_availability  = var.high_availability
+  maintenance_window = var.maintenance_window
+
+  # depends_on = [azurerm_postgresql_flexible_server.example]
+}
+
+resource "azurerm_postgresql_flexible_server_database" "database" {
+  for_each  = var.databases
+  name      = each.key
+  server_id = azurerm_postgresql_flexible_server.postgresql_server.id
+  collation = lookup(each.value, "collation", local.default_collation)
+  charset   = lookup(each.value, "charset", local.default_charset)
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "server_config" {
+  for_each  = var.server_config
+  name      = each.key
+  server_id = azurerm_postgresql_flexible_server.postgresql_server.id
+  value     = each.value
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "firewall_rules" {
+  for_each         = var.firewall_rules
+  name             = each.key
+  server_id        = azurerm_postgresql_flexible_server.postgresql_server.id
+  start_ip_address = each.value[0]
+  end_ip_address   = each.value[1]
+}
+
+
---
+++ vars.tf
@@ -0,0 +1,120 @@
+variable databases {
+  type        = object({
+    collation = optional(string)
+    charset = optional(string)
+  })
+  default     = {}
+  description = "Dictionary of databases"
+}
+
+variable "server_version" {
+  description = "Specifies the version of PostgreSQL to use."
+  type        = string
+  default     = "14"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which to create the PostgreSQL Server. Changing this forces a new resource to be created."
+}
+
+variable name {
+  type        = string
+  description = "Specifies the name of the PostgreSQL Server. Changing this forces a new resource to be created."
+}
+
+variable delegated_subnet_id {
+  type        = string
+  default     = null
+  description = "Delegated subnet ID"
+}
+
+variable private_dns_zone_id {
+  type        = string
+  default     = null
+  description = "Private DNS zone ID"
+}
+
+variable location {
+  type        = string
+  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+}
+
+variable "administrator_login" {
+  description = "The Administrator Login for the PostgreSQL Server. Changing this forces a new resource to be created."
+  default     = "pgadm"
+  type        = string
+}
+
+variable "administrator_password" {
+  type        = string
+  description = "The Password associated with the administrator_login for the PostgreSQL Server."
+}
+
+variable "sku_name" {
+  description = "Specifies the SKU Name for this PostgreSQL Server. The name of the SKU, follows the tier + family + cores pattern (e.g. B_Gen4_1, GP_Gen5_8)."
+  type        = string
+  default     = "GP_Standard_D2ds_v4"
+}
+
+variable "storage_mb" {
+  description = "Max storage allowed for a server. Possible values are between 32768 MB(32GB) and 16777216 MB(16TB)."
+  default     = 32768
+}
+
+variable zones {
+  description = "Specifies the Availability Zone in which the PostgreSQL Flexible Server should be located."
+}
+
+variable tags {
+  type        = string
+  description = "A mapping of tags which should be assigned to the PostgreSQL Flexible Server."
+}
+
+variable high_availability {
+  type        = object({
+    mode = optional(string)
+    standby_availability_zone = optional(string)
+  })
+  default     = null
+  description = "mode - (Required) The high availability mode for the PostgreSQL Flexible Server. The only possible value is ZoneRedundant. 
+                 standby_availability_zone - (Optional) Specifies the Availability Zone in which the standby Flexible Server should be located."
+}
+
+variable maintenance_window {
+    type        = object({
+    day_of_week = optional(string)
+    start_hour = optional(string)
+    start_minute = optional(string)
+  })
+  default     = null
+  description = "day_of_week - (Optional) The day of week for maintenance window, where the week starts on a Sunday, i.e. Sunday = 0, Monday = 1. Defaults to 0.
+                 start_hour - (Optional) The start hour for maintenance window. Defaults to 0.
+                 start_minute - (Optional) The start minute for maintenance window. Defaults to 0."
+}
+
+variable geo_redundant_backup_enabled {
+  type        = bool
+  default     = false
+  description = "geo_redundant_backup_enabled - (Optional) Is Geo-Redundant backup enabled on the PostgreSQL Flexible Server. Defaults to false. Changing this forces a new PostgreSQL Flexible Server to be created."
+}
+
+variable backup_retention_days {
+  type        = number
+  default     = 30
+  description = "- (Optional) The backup retention days for the PostgreSQL Flexible Server. Possible values are between 7 and 35 days."
+}
+
+variable firewall_rules {
+    type = map(list(string))
+    default     = {}
+    description = "Manages a PostgreSQL Flexible Server Firewall Rule."
+}
+
+variable server_config {
+    type = map(string)
+    description = "Sets a PostgreSQL Configuration value on a Azure PostgreSQL Flexible Server."
+}
+
+
+
