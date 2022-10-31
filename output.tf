dde3cae3d1e99d6831af58d11611e31e24227952
Marcel Grygar <marcel.grygar@tietoevry.com>
2022-10-31 09:05:01 +0100

    Adding output

---
+++ output.tf
@@ -0,0 +1,16 @@
+output "server_name" {
+  description = "The name of the PostgreSQL server"
+  value       = azurerm_postgresql_flexible_server.postgresql_server.name
+}
+
+output "server_fqdn" {
+  description = "The fully qualified domain name (FQDN) of the PostgreSQL server"
+  value       = azurerm_postgresql_flexible_server.fqdn
+}
+
+output "server_id" {
+  description = "The resource id of the PostgreSQL server"
+  value       = azurerm_postgresql_flexible_server.postgresql_server.id
+}
+
+
